9542a74a3bd7d67ac52dd6924423162a123a1e23
vuvlnvw <vuvlnvw@gmail.com>
2024-07-27 05:57:14 +0000

    [update @temporary] button page

--- src/routes/(component)/button/+page.svelte
+++ src/routes/(component)/button/+page.svelte
@@ -1,27 +1,51 @@
 <script lang="ts">
 	import { Button, Spacer, Loader } from '@components';
 	import { ShieldQuestion, Check, AlertCircle } from 'lucide-svelte';
+	import { fade } from 'svelte/transition';
+
+	const buttons: Record<
+		string,
+		{
+			name: string;
+			collor?: collorType;
+			variant?: string[];
+			size?: string;
+			icon?: any;
+		}[]
+	> = {
+		varian: [
+			{ name: 'main', collor: 'main' },
+			{ name: 'success', collor: 'success' },
+			{ name: 'primary', collor: 'primary' }
+		],
+		size: [
+			{ name: 'smaller', collor: 'main', size: 'sm' },
+			{ name: 'default', collor: 'success', size: 'md' },
+			{ name: 'bigger', collor: 'primary', size: 'lg' }
+		],
+		icon: [],
+		style: []
+	};
 </script>
 
-<div class="flex flex-col items-start justify-center gap-4">
-	<div class="group">
-		<p class="title">Button Varian</p>
-		<Spacer />
-		<div class="item">
-			<Button collor="main">Button Main</Button>
-			<Button collor="success">Button Success</Button>
-			<Button collor="primary">Button Primary</Button>
+<div in:fade={{ delay: 100, duration: 150 }} class="flex w-full flex-col items-start justify-center gap-4 xl:flex-row">
+	{#each Object.keys(buttons) as key}
+		<div class="flex w-full flex-col items-center justify-center border">
+			<a href="#{key}" class="w-full py-2 pl-3 text-lg font-extrabold capitalize text-foreground/80">{key}</a>
+			<div class="w-full space-y-2 border-t p-3">
+				{#each buttons[key] as { name, collor, size }}
+					<div class="flex gap-3">
+						<Button {collor}>Button</Button>
+						<p class="text-xs font-semibold">{name}</p>
+					</div>
+				{/each}
+			</div>
 		</div>
-	</div>
-	<div class="group">
-		<p class="title">Button Size</p>
-		<Spacer />
-		<div class="item">
-			<Button collor="main" size="smaller">Small</Button>
-			<Button collor="success" size="default">Default</Button>
-			<Button collor="primary" size="bigger">Bigger</Button>
-		</div>
-	</div>
+	{/each}
+</div>
+
+<!-- <div class="hidden flex flex-col items-start justify-center gap-4">
+
 
 	<div class="group">
 		<p class="title">Button Icon</p>
@@ -47,7 +71,7 @@
 			<Button collor="main"><Loader text="Fetching" /></Button>
 		</div>
 	</div>
-</div>
+</div> -->
 
 <style lang="postcss">
 	.title {
