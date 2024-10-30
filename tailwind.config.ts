b186e816a04173a47d518e900a85ba4b6e367057
amarosuli <vuvlnvw@gmail.com>
2024-10-30 12:11:56 +0700

    [new] Initial Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import aspectRatio from '@tailwindcss/aspect-ratio';
-import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
@@ -11,5 +10,5 @@ export default {
     extend: {}
   },
 
-  plugins: [typography, forms, containerQueries, aspectRatio]
+  plugins: [typography, forms, aspectRatio]
 } satisfies Config;
